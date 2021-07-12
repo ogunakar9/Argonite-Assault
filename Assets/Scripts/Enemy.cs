45af0fe9515fc68196808fa4cd63d19afa8c9529
ogunakar9 <ogunakar09@gmail.com>
2021-07-12 15:46:13 +0300

    Add enemy waves, UI elements for image and audio of a character

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,7 +7,7 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject deathVFX;
     [SerializeField] private GameObject hitVFX;
-    [SerializeField] int scorePerHit = 15;
+    [SerializeField] int scorePerDeath = 15;
     [SerializeField] int hitPoints = 4;
     
     private ScoreBoard scoreBoard;
@@ -42,11 +42,11 @@ public class Enemy : MonoBehaviour
         GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parentGameObject.transform;
         hitPoints--;
-        scoreBoard.IncreaseScore(scorePerHit);
     }
 
     private void KillEnemy()
     {
+        scoreBoard.IncreaseScore(scorePerDeath);
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
